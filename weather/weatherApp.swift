dd7ae66c441fa39dacff0d8aa7d8f821c71509c2
tarekahmed199 <tarek.ahmed@nandbox.com>
2023-03-11 17:04:10 +0200

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by lapshop on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
